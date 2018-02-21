75ff7dfb85a43e8190d115a116adc9f7b2e26d5d
manueltrialrosa <36444170+manueltrialrosa@users.noreply.github.com>
2018-02-21 03:58:13 -0500

    Update customer_service.js

--- public/js/service/customer_service.js
+++ public/js/service/customer_service.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp').factory('CreditService', ['$http', '$q', function($http, $q){
 
-  var REST_SERVICE_URI = './creditscoreV4';
+  var REST_SERVICE_URI = './creditscoreV1';
 
   var factory = {
     creditScore: creditScore,
